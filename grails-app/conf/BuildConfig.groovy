f3cfbb687b6c39fa906b83b88dda0c5590342c95
Matt Deimel <mdeimel@gmail.com>
2014-07-30 10:25:58 -0500

    Update to work with grails 2.4.2
    Removed references to PluginManagerHolder (removed in Grails 2.4), and
    replaced with Holders.getPluginManager()
    
    Updated the jquery plugin from 1.8.2 to 1.11.0.2, as recommended here:
    http://grails.org/doc/2.4.2/guide/upgradingFrom23.html

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
 
 	plugins {
 		compile ':google-visualization:0.6'
-		compile ':jquery:1.8.2'
+		compile ':jquery:1.11.0.2'
 
 		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
 			export = false
